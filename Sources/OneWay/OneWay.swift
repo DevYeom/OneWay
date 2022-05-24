6e3f89e489196960270b8b98bb63d483f3fdaa24
DevYeom <dev.yeom@gmail.com>
2022-05-24 20:53:19 +0900

    Add some comments

--- Sources/OneWay/OneWay.swift
+++ Sources/OneWay/OneWay.swift
@@ -3,30 +3,46 @@ import Combine
 
 open class Way<Action, State> {
 
+    /// Determines which thread environment Way will be working on.
     public enum ThreadOption {
+        /// Run on current thread. It needs to ensure that all actions run on the same thread.
         case current
+
+        /// It guarantees Way is thread-safe. Use only when absolutely necessary.
         case threadSafe
     }
 
+    /// The initial state.
     public let initialState: State
+
+    /// The current state.
     public var currentState: State { stateSubject.value }
+
+    /// A publisher that emits when state changes.
     public var publisher: WayPublisher<State> {
         WayPublisher(way: self)
     }
 
     internal var stateSubject: CurrentValueSubject<State, Never>
 
-    private let threadOption: ThreadOption
-
+    /// Use only when thread-safe mode.
     private var actionSubject: PassthroughSubject<Action, Never>!
+
+    /// Use only when thread-safe mode.
     private let actionQueue: DispatchQueue!
 
+    private let threadOption: ThreadOption
     private var isSending: Bool = false
     private var bufferedActions: [Action] = []
     private var sideWayCancellables: [UUID: AnyCancellable] = [:]
     private var cancellables = Set<AnyCancellable>()
     private var bindCancellable: AnyCancellable?
 
+    /// Initializes a way from an initial state, threadOption.
+    ///
+    /// - Parameters:
+    ///   - initialState: The state to start the application in.
+    ///   - threadOption: The option to determine thread environment. Default value is `current`
     public init(initialState: State, threadOption: ThreadOption = .current) {
         self.initialState = initialState
         self.threadOption = threadOption
@@ -61,14 +77,26 @@ open class Way<Action, State> {
             })
     }
 
+    /// Evolves the current state of an application to the next state.
+    ///
+    /// - Parameters:
+    ///   - state: The current state of the application.
+    ///   - action: The action that causes the current state of the application to change.
     open func reduce(state: inout State, action: Action) -> SideWay<Action, Never> {
         return .none
     }
 
+    /// Binds the global states to causes the current state of the application to change.
+    ///
+    /// - Returns: A sideWay to deliver an action.
     open func bind() -> SideWay<Action, Never> {
         return .none
     }
 
+    /// Sends an action to a Way.
+    ///
+    /// - Parameters:
+    ///   - action: An action to perform `reduce(state:action:)`
     public func send(_ action: Action) {
         switch threadOption {
         case .current:
@@ -114,6 +142,9 @@ open class Way<Action, State> {
 
 }
 
+/// A publisher of Way's state.
+///
+/// This pulisher supports dynamic member lookup so that you can pluck out a specific field in the state.
 @dynamicMemberLookup
 public struct WayPublisher<State>: Publisher {
     public typealias Output = State

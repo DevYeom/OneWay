f7833f19900fd065e34dc7683be16d05a7ca70f6
DevYeom <dev.yeom@gmail.com>
2022-06-12 21:00:22 +0900

    Change internal AnyWay to public

--- Sources/OneWay/AnyWay.swift
+++ Sources/OneWay/AnyWay.swift
@@ -1,21 +1,16 @@
 import Foundation
 import Combine
 
-internal protocol OneWay: AnyObject {
+public protocol AnyWay: AnyObject {
     associatedtype Action
-
     associatedtype State
 
     var initialState: State { get }
-
     var currentState: State { get }
-
     var publisher: WayPublisher<State> { get }
 
     func reduce(state: inout State, action: Action) -> SideWay<Action, Never>
-
     func bind() -> SideWay<Action, Never>
-
     func send(_ action: Action)
 }
 
